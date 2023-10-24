943b5d1829d6f0640913f56f3ef6f33789caa124
Khalil Hammami <khalil.hammami@enetcom.usf.tn>
2023-10-24 16:30:24 +0100

    first commit

---
+++ app/src/main/java/com/example/enetcom/recyclerview/data/Datasource.java
@@ -0,0 +1,18 @@
+package com.example.enetcom.recyclerview.data;
+
+import com.example.enetcom.recyclerview.model.Word;
+
+import java.util.LinkedList;
+
+public class Datasource {
+    public Datasource(){}
+
+
+    public LinkedList<Word> loadWords(){
+        LinkedList<Word> wordList = new LinkedList<>();
+        for (int i = 0; i < 20; i++) {
+            wordList.addLast(new Word("Word " + i));
+        }
+        return wordList;
+    }
+}
